8caac57daef3607203ec25a0ed89aa30878f9f61
unknown <james.rowland@ccc.ox.ac.uk>
2019-03-06 01:08:14 +0000

    further dev of packio file extraction for sta etc

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import numpy as np
 import tifffile as tf
 import ntpath
 import os
+from lxml import objectify
+from lxml import etree
 
 
 
@@ -27,6 +29,8 @@ def interpolate_through_stim(stack=None, db=None, threshold=1.5):
     if stack: returns artifact cleaned stack array
     if db: returns db dict with path now to cleaned tiff
          
+    (JR 2019) 
+     
     '''
 
     if not stack and not db: raise ValueError('must pass function stack or db')
@@ -84,7 +88,88 @@ def interpolate_through_stim(stack=None, db=None, threshold=1.5):
     else:
         return stack
 
+        
+        
+class ParseMarkpoints():  
+    
+    '''
+    parses gpls or xmls from markpoints into python variables
+    copied from Blimp (JR 2019)
+    
+    '''
 
+    def __init__(self, xml_path=None, gpl_path=None):
+        
+        #super(ParseMarkpoints, self).__init__()
+        
+        self.xml_path = xml_path
+        self.gpl_path = gpl_path
+        
+        if self.gpl_path:            
+            self.parse_gpl()
+            
+        if self.xml_path:
+            self.parse_xml()
+
+       
+        
+    def parse_gpl(self):
+        
+        '''extract information from naparm gpl file'''
+        
+        gpl = etree.parse(self.gpl_path)
+
+        self.Xs = []
+        self.Ys = []
+        self.is_spirals = []
+        self.spiral_sizes = []
+
+        for elem in gpl.iter():
+
+            if elem.tag == 'PVGalvoPoint':
+                self.Xs.append(elem.attrib['X'])
+                self.Ys.append(elem.attrib['Y'])
+                self.is_spirals.append(elem.attrib['IsSpiral'])
+                #the spiral size gpl attrib contains a positive and negative number for 
+                #some reason. This takes just the positive, may cause errors later
+                self.spiral_sizes.append(elem.attrib['SpiralSize'].split(' ')[0])
+                
+            
+    def parse_xml(self):
+
+        '''extract requried information from xml into python lists'''
+
+        xml = etree.parse(self.xml_path)
+
+        self.laser_powers = []
+        self.spiral_revolutions = []
+        self.durations = []
+        self.initial_delays = []
+        self.repetitions = []
+        
+        for elem in xml.iter():
+
+            if elem.tag == 'PVSavedMarkPointSeriesElements':
+                self.iterations = elem.attrib['Iterations']
+            
+            if elem.tag == 'PVMarkPointElement':
+                self.laser_powers.append(elem.attrib['UncagingLaserPower'])
+                self.repetitions.append(elem.attrib['Repetitions'])
+                
+            elif elem.tag == 'PVGalvoPointElement':
+                self.durations.append(elem.attrib['Duration'])
+                self.spiral_revolutions.append(elem.attrib['SpiralRevolutions'])
+                self.initial_delays.append(elem.attrib['InitialDelay'])
+                
+               
+        #detect if a dummy point has been sent and remove it if so
+        if self.laser_powers[0] == '0':
+            self.dummy = True
+            self.laser_powers.pop(0)
+            self.spiral_revolutions.pop(0)
+            self.durations.pop(0)
+            self.initial_delays.pop(0)
+            self.repetitions.pop(0)
 
 
 
