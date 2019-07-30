9e9824b821b6ddf6f5475bbb5b8913d6220050e8
James Rowland <james.rowland@ccc.ox.ac.uk>
2019-07-30 18:54:05 +0100

    added subsapce preprocessing notebook

--- utils/utils_funcs.py
+++ utils/utils_funcs.py
@@ -40,6 +40,7 @@ def dfof(arr):
 
 
 def dfof2(flu):
+
     '''
     delta f over f, this function is orders of magnitude faster than the dumb one above
     takes input matrix flu (num_cells x num_frames)
@@ -78,6 +79,8 @@ def s2p_loader(s2p_path, subtract_neuropil=True, neuropil_coeff = 0.7):
                 is_cells = np.load(os.path.join(root, file))[:, 0]
                 is_cells = np.ndarray.astype(is_cells, 'bool')
                 print('Loading {} traces labelled as cells'.format(sum(is_cells)))
+            elif file == 'spks.npy':
+                spks = np.load(os.path.join(root, file))
             elif file == 'stat.npy':
                 stat = np.load(os.path.join(root, file))
                 found_stat = True
@@ -90,13 +93,16 @@ def s2p_loader(s2p_path, subtract_neuropil=True, neuropil_coeff = 0.7):
 
     stat = stat[is_cells]
 
+    spks = spks[is_cells,:]
+
     if not subtract_neuropil:
-        return all_cells[is_cells, :], stat
+        return all_cells[is_cells, :],  spks, stat
+
 
     else:
         print('Subtracting neuropil with a coefficient of {}'.format(neuropil_coeff))
         neuropil_corrected = all_cells - neuropil * neuropil_coeff
-        return neuropil_corrected[is_cells, :], stat
+        return neuropil_corrected[is_cells, :], spks, stat
 
 
 def correct_s2p_combined(s2p_path):
@@ -211,5 +217,126 @@ def myround(x, base=5):
     return base * round(x/base)
 
 
+def tseries_finder(tseries_lens, frame_clock, paq_rate=20000):
 
+    '''Finds chunks of frame clock that correspond to the tseries in tseries lens
+
+       tseries_lens -- list of the number of frames each tseries you want to find 
+                       contains
+       frame_clock  -- thresholded times each frame recorded in paqio occured
+       
+       paq_rate     -- input sampling rate of paqio
+
+       '''
+
+    # frame clock recorded in paqio, includes TTLs from cliking 'live' and foxy extras 
+    clock = frame_clock / paq_rate 
+
+    # break the recorded frame clock up into individual aquisitions
+    # where TTLs are seperated by more than 1s
+    gap_idx = np.where(np.diff(clock) > 1)
+    gap_idx = np.insert(gap_idx, 0, 0)
+    gap_idx = np.append(gap_idx, len(clock))
+    chunked_paqio = np.diff(gap_idx)
+
+    # are each of the frames recorded by the frame clock actually in processed tseries?
+    real_frames = np.zeros(len(clock))
+    # the max number of extra frames foxy could spit out
+    foxy_limit = 20
+    # the number of tseries blocks that have already been found
+    series_found = 0 
+    # count how many frames have been labelled as real or not
+    counter = 0 
+
+    for chunk in chunked_paqio:
+        is_tseries = False
+        
+        # iterate through the actual length of each analysed tseries
+        for idx, ts in enumerate(tseries_lens):
+            # ignore previously found tseries
+            if idx < series_found: 
+                continue
+                
+            # the frame clock block matches the number of frames in a tseries
+            if chunk >= ts and chunk <= ts + foxy_limit:            
+                # this chunk of paqio clock is a recorded tseries
+                is_tseries = True            
+                # advance the number of tseries found so they are not detected twice 
+                series_found +=1      
+                break
+        
+        if is_tseries:
+            # foxy bonus frames
+            extra_frames = chunk - ts
+            # mark tseries frames as real
+            real_frames[counter:counter+ts] = 1
+            # move the counter on by the length of the real tseries
+            counter += ts
+            # set foxy bonus frames to not real
+            real_frames[counter:counter+extra_frames] = 0
+            # move the counter along by the number of foxy bonus frames
+            counter += extra_frames
+            
+        else:
+            # not a tseries so just move the counter along by the chunk of paqio clock
+            counter += chunk  + 1 # this could be wrong, not sure if i've fixed the ob1 error, go careful
+                
+    real_idx = np.where(real_frames==1)
+
+    return frame_clock[real_idx]
+
+
+
+def flu_splitter(flu, clock, t_starts, pre_frames, post_frames):
+
+    '''Split a fluoresence matrix into trial by trial array
+
+       flu -- fluoresence matrix [num_cells x num_frames]
+       clock -- the time that each frame occured
+       t_starts -- the time each frame started
+       pre_frames -- the number of frames before t_start
+                     to include in the trial
+       post_frames --  the number of frames after t_start
+                       to include in the trial
+       
+       returns 
+       trial_flu -- trial by trial array 
+                    [num_cells x trial frames x num_trials]
+
+       n.b. clock and t_start must have same units and
+            reference frame (see rsync_aligner.py
+
+       '''
+
+    assert flu.shape[1] == len(clock), '{} frames in fluorescent array '\
+                                       '{} frames in clock'\
+                                       .format(flu.shape[1], len(clock))
+    
+    num_trials = len(t_starts)
+    imaging_trial = [False] * num_trials # was the trial imaged?
+    
+    first = True # dumb way of building array in for loop
+    for trial, t_start in enumerate(t_starts):
+        # the trial occured before imaging started 
+        if t_start < min(clock): 
+            continue
+            
+        # find the first frame occuring after each trial start
+        for idx, frame in enumerate(clock):
+            # the idx the frame immediiately proceeds the t_start
+            if frame - t_start >= 0:
+            
+                imaging_trial[trial] = True
+                flu_chunk = flu[:,idx-pre_frames:idx+post_frames]
+                
+                if first:
+                    trial_flu = flu_chunk
+                    first = False
+                else:
+                    trial_flu = np.dstack((trial_flu, flu_chunk))                
+                break 
+
+    assert trial_flu.shape[2] == sum(imaging_trial)
+
+    return trial_flu
 
