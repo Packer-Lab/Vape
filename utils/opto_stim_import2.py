f9afb72bb2c2591f045e8e0f7838e3f9f7443550
James Rowland <james.rowland@ccc.ox.ac.uk>
2019-08-05 15:56:54 +0100

    forgot to add opto_stim_import2.py to previous commit

--- utils/opto_stim_import2.py
+++ utils/opto_stim_import2.py
@@ -9,6 +9,7 @@ from utils_funcs import d_prime as pade_dprime
 import gsheets_importer as gsheet
 from paq2py import paq_read
 from rsync_aligner import Rsync_aligner
+import re
 warnings.filterwarnings("ignore")
 
 
@@ -219,28 +220,30 @@ class OptoStim2p(OptoStimBasic):
         super().__init__(txt_path)
 
         #the line that triggers an SLM trial throuh blimp
-        _slm_trigger_lines = [line for line in self.print_lines if 'Trigger SLM trial Number' in line]
+        _slm_trigger_lines = [line for line in self.print_lines if 'Trigger SLM trial' in line]
         _nogo_trigger_lines = [line for line in self.print_lines if 'Trigger NOGO trial' in line]
-        _alltrials_trigger_lines = [line for line in self.print_lines if 'Trigger SLM trial Number' in line or 'Trigger NOGO trial' in line]
+        _alltrials_trigger_lines = [line for line in self.print_lines if 'Trigger SLM trial' in line or 'Trigger NOGO trial' in line]
+         
+        self.slm_barcode = [re.search('(?<=Barcode )(.*)', line).group(0) for line in _slm_trigger_lines]
+        self.slm_trial_number = [re.search('(?<=Number )(.*)(?= Barcode)', line).group(0) for line in _slm_trigger_lines]
 
-        self.slm_barcode = [float(line.split(' ')[7]) for line in _slm_trigger_lines]
-        self.slm_trial_number = [float(line.split(' ')[5]) for line in _slm_trigger_lines]
+        self.nogo_barcode = [re.search('(?<=Barcode )(.*)', line).group(0) for line in _nogo_trigger_lines]
+        self.nogo_trial_number = [re.search('(?<=Number )(.*)(?= Barcode)', line).group(0) for line in _nogo_trigger_lines]
 
-        self.nogo_barcode = [float(line.split(' ')[7]) for line in _nogo_trigger_lines]
-        self.nogo_trial_number = [float(line.split(' ')[5]) for line in _nogo_trigger_lines]
-
-        self.alltrials_barcodes = [float(line.split(' ')[7]) for line in _alltrials_trigger_lines]
+        self.alltrials_barcodes = [re.search('(?<=Barcode )(.*)', line).group(0) for line in _alltrials_trigger_lines]
 
         assert len(self.slm_barcode) == self.trial_type.count('go')
-        #necessary to analyse sessions without nogo blimping
+        # necessary to analyse sessions without nogo blimping
         if len(self.nogo_barcode) > 0:
             assert len(self.nogo_barcode) == self.trial_type.count('nogo')
 
         self.rsync = self.session.times.get('rsync')
-        go_start = self.session.times.get('SLM_state')
+        # go_start = self.session.times.get('SLM_state')
         nogo_start = self.session.times.get('nogo_state')
-        ts = np.sort(np.hstack((go_start, nogo_start)))
+        easy_start  = self.session.times.get('easy_state')
+        test_start  = self.session.times.get('test_state')
 
+        ts = np.sort(np.hstack((easy_start, test_start, nogo_start)))
         self.trial_start = self.test_import_and_slice(ts) 
      
 
@@ -250,32 +253,35 @@ class BlimpImport(OptoStim2p):
     sheet_ID = '1GG5Y0yCEw_h5dMfHBnBRTh5NXgF6NqC_VVGfSpCHroc'
     packerstation_path = '/home/jamesrowland/Documents/packerstation/jrowland/Data'
 
-    #the name of the file path column
+    # the column headers of the spreadsheet 
     date_header = 'Date'
     tseries_header = 't-series name'
     paq_header = '.paq file'
     naparm_header = 'Naparm'
     blimp_header = 'blimp folder'
     pycontrol_header = 'pycontrol txt'
-
-    #column telling whether to analyse or not
+    plane_header = 'Number of planes'
     analyse_bool_header = 'Analyse'
 
     def __init__(self, mouse_id):
-        '''
-        class to import 2-photon blimp experiments based on the
-        gsheet given in sheet ID
-        '''
+
+        '''class to import 2-photon and 1-photon blimp experiments based on the
+           gsheet given in sheet ID
+
+           '''
+
         self.mouse_id = mouse_id
-        _sheet_name = self.mouse_id + '!A1:Z69'
+        _sheet_name = self.mouse_id + '!A1:ZZ69'
         self.df = gsheet.gsheet2df(BlimpImport.sheet_ID, HEADER_ROW=2, SHEET_NAME=_sheet_name)
         self.df = gsheet.correct_behaviour_df(self.df)
         self.parse_spreadsheet()
 
     def parse_spreadsheet(self):
     
-        ''' parses the spreadsheet for dealing with 2p behaviour
-            use import_1p for 1-photon behaviour'''
+        '''parses the spreadsheet for dealing with 2p behaviour
+           use import_1p for 1-photon behaviour
+            
+           '''
 
         idx_analyse = gsheet.df_bool(self.df, BlimpImport.analyse_bool_header)
         idx_2p = gsheet.df_bool(self.df, 'Trained 2P')
@@ -292,12 +298,14 @@ class BlimpImport(OptoStim2p):
         self.blimp_folders = gsheet.df_col(self.df, BlimpImport.blimp_header, self.rows_2p)
         self.pycontrol_folders = gsheet.df_col(self.df, BlimpImport.pycontrol_header, self.rows_2p)
         self.tseries_folders = gsheet.df_col(self.df, BlimpImport.tseries_header, self.rows_2p)
-        
+        self.plane_numbers = gsheet.df_col(self.df, BlimpImport.plane_header, self.rows_2p)
 
         assert len(self.paqs) == len(self.naparm_folders) == len(self.blimp_folders) == len(self.dates_2p)
         num_runs = len(self.paqs)
 
     def import_1p(self):
+
+        '''call me instead of get_object_and_test if importing 1-photon experiment''' 
         
         dates_1p = gsheet.df_col(self.df, BlimpImport.date_header, self.rows_1p)
         pycontrol_1p = gsheet.df_col(self.df, BlimpImport.pycontrol_header, self.rows_1p)
@@ -311,18 +319,20 @@ class BlimpImport(OptoStim2p):
 
         return obj_list
 
-            #print(self.pycontrol_path)
-
-       
 
     def get_object_and_test(self, run, raise_error=True):
 
-        '''build object for a sepcific run and test that details have been entered correctly
-           this is currently implemented in a way that is useful for just getting behavioural information
-           and processing it in a seperate script.
-           The paq and alignment file is just used to check that all the paths etc are good. When i start to
-           use alignment and paq this function will need to be reworked
-        '''
+        '''Build object for a sepcific run and test that details have been entered correctly
+
+           Adds attributes to the BlimpImport object from the paq file, and blimp alignment file
+           also generates an alignment object using rsync that can be used to map paq events
+           to pycontrol txt file events
+           Inputs:
+           run -- run number to process, matching metadata spreadsheet
+           raise_error -- whether to raise an error or just print if the paq or blimp alignment file could
+                          not be matched to the pycontrol txt file
+
+           '''
 
         self.run_pycontrol_txt = self.df.loc[self.df['Run Number'] == str(run)]['pycontrol txt'].tolist()[0]
 
@@ -334,26 +344,27 @@ class BlimpImport(OptoStim2p):
         blimp = self.blimp_folders[run_idx]
         pycontrol = self.pycontrol_folders[run_idx]
         tseries = self.tseries_folders[run_idx]
+        self.num_planes = int(self.plane_numbers[run_idx])
 
         umbrella = os.path.join(BlimpImport.packerstation_path, date)
         self.blimp_path, self.naparm_path  = gsheet.path_finder(umbrella, blimp, naparm, is_folder=True)
         self.pycontrol_path, self.paq_path = gsheet.path_finder(umbrella, pycontrol, paq, is_folder=False)
         
         self.tseries_paths = []
-        for t in tseries:
+        for t in tseries: 
             self.tseries_paths.append(gsheet.path_finder(umbrella, t, is_folder=True))
 
         with open(os.path.join(self.blimp_path, 'blimpAlignment.txt'), 'r') as f:
             file_lines = f.readlines()
-            self.align_barcode = [float(line.split(' ')[7][:-1]) for line in file_lines] #take the full stop off the end
+            self.align_barcode = [re.search('(?<=Barcode )(.*)(?=. Info)', line).group(0) for line in file_lines] 
             self.trial_info = [line.split('Info:')[-1].rstrip('\n').strip() for line in file_lines]
 
-        #build behaviour object from the pycontrol txt file
+        # build behaviour object from the pycontrol txt file
         super().__init__(self.pycontrol_path)
-        #test that the list of barcodes printed in the pycontrol sequence is contained in the
-        #list of barcodes from the alignment folder
+        # test that the list of barcodes printed in the pycontrol sequence is contained in the
+        # list of barcodes from the alignment folder
         if not ''.join([str(b) for b in self.alltrials_barcodes]) in ''.join([str(b) for b in self.align_barcode]):
-            error_str = 'pycontrol {} does not match blimp folder {}'
+            error_str = 'pycontrol {} does not match blimp folder {}'.format(pycontrol, blimp)
             if raise_error: raise ValueError(error_str)
             else: print(error_str)
         else:
@@ -372,12 +383,23 @@ class BlimpImport(OptoStim2p):
             self.aligner = Rsync_aligner(pulse_times_A=self.rsync, pulse_times_B=self.paq_rsync,
                                     units_B=1000/_paq_obj['rate'], chunk_size=6, plot=False, raise_exception=True)
             self.paq_correct = True
-            self.testytest = True
             print('pycontrol {} rsync successfully matched to paq {}'.format(pycontrol, paq))
         except:
             self.paq_correct = False
             error_str = 'pycontrol rsync does not match paq'
-            if raise_error: raise ValueError(error_str)
+            if raihe_error: raise ValueError(error_str)
             else: print(error_str)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
