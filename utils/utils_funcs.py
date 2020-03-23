1faed5aae9e64f801357c67eee07f1e62e5504ae
neuromantic99 <james.rowland@ccc.ox.ac.uk>
2020-03-23 16:33:19 +0000

    remove debug from threshold_detect

--- utils/utils_funcs.py
+++ utils/utils_funcs.py
@@ -161,11 +161,6 @@ def threshold_detect(signal, threshold):
     '''lloyd russell'''
     thresh_signal = signal > threshold
     thresh_signal[1:][thresh_signal[:-1] & thresh_signal[1:]] = False
-
-
-    thresh_signal[200:][thresh_signal[:-200] & thresh_signal[200:]] = False
-
-
     times = np.where(thresh_signal)
     return times[0]
 
